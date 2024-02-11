ba61a47305a9778526a8eb80621d5b6c449f0c59
tnem <tanmay.e.mahale@gmail.com>
2024-02-11 08:48:00 +0530

    Solved-mongo-issue

--- server.js
+++ server.js
@@ -20,12 +20,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   // .connect(process.env.DATABASE_LOCAL, {
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  .connect(DB)
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
